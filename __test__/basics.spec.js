785d10b3c9f10d34042144694e8b5eef5fdcd466
Harry MacCode <emarh.harry.code@gmail.com>
2024-04-08 12:55:57 +0200

    Test a null varaible

--- __test__/basics.spec.js
+++ __test__/basics.spec.js
@@ -1,3 +1,13 @@
 test('addition', () => {
     expect(2+3).toBe(5);
 });
+
+test('null', () => {
+    const i = null;
+
+    // Number of expect in the test
+    expect.assertions(2);
+
+    expect(i).toBeNull();
+    expect(i).toBeDefined();
+});
