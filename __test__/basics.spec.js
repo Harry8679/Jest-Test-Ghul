073e105652f0595576d5f368643fde63abfbd3b0
Harry MacCode <emarh.harry.code@gmail.com>
2024-04-08 13:28:24 +0200

    Return throw Not Nul

--- __test__/basics.spec.js
+++ __test__/basics.spec.js
@@ -11,3 +11,18 @@ test('null', () => {
     expect(i).toBeNull();
     expect(i).toBeDefined();
 });
+
+const animals = ['cat', 'dog'];
+
+test('Animal Array', () => {
+    expect(animals).toContain('cat');
+    expect(animals).toBeInstanceOf(Array);
+});
+
+const getData = () => {
+    throw new Error('Not Found');
+};
+
+test('getData', () => {
+    expect(() => getData()).toThrow('Not Found');
+});
